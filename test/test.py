186be4cbeebddea30f93793453bbc1c91cfe6d03
Edwin Lawuiekpor <elawuiekpor@zedulo.com>
2026-03-16 20:50:32 +0000

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -26,15 +26,17 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    #dut.ui_in.value = 20
+    #dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # Change it to match the actual expected output of your module, tmp:
+    assert dut.uo_out.value.is_resolvable, "uo_out contains X or Z"
+    assert dut.uio_out.value.is_resolvable, "uio_out contains X or Z"
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+    dut._log.info("smoke test passed")
